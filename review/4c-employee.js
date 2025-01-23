e4a43a1a45dbeaf9e5408103ef43147d6c22d57e
Nikos Baxevanis <nikosbaxevanis@MacBookAir.home>
2025-01-23 21:00:31 +0200

    23/01/2025

---
+++ review/4c-employee.js
@@ -0,0 +1,13 @@
+const employee = {
+  fistname: 'bob' ,
+  position: 'Developer',
+  address : {
+    street : 'Patision 75',
+    city : 'Athens',
+    zipCode : '10434'
+  }
+}
+
+console.log(employee.address.street)
+console.log(employee.address.city)
+console.log(employee.address.zipCode)
---
+++ review/4e-objectWithMethod.js
@@ -0,0 +1,14 @@
+const calc = {
+  add: function (a,b) {
+    return a+b
+  },
+
+  sub(a,b) {
+    return a-b
+  }
+}
+
+
+let a = 2 
+let b = 3
+console.log(calc.add(a,b), calc.sub(a,b))
