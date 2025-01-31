09a2d12b5768720af92babb7fc95d6b99cd0e7b0
Nikos Baxevanis <baxemb@gmail.com>
2025-01-31 19:13:01 +0200

    31/01/2025

---
+++ arrays/16-iteratorMethods.js
@@ -0,0 +1,15 @@
+const students = ['Alice' , 'Andreas' , 'Bob', 'Costas']
+
+students.forEach(function(stu , _, arr) {
+  console.log(stu  ,arr)
+})
+
+let filtered = students.filter(student => student === 'Andreas') //Δημιουργει ενα νεο πινακα που περιεχει μονο το 'Andreas'
+console.log(filtered)
+
+let mapped = students.map(student => "Student: " +student) //Προσθετει στο πινακα το λεκτικο student σε καθε θεση
+console.log(mapped)
+
+const numbers = [1,6,9,12]
+let sum = numbers.reduce((total,val) => total + val ,0)
+console.log(sum)
